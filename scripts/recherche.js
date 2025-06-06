f3018cfcfd7487fdb9d9ce71910417318a47aa5f
nchevalier44 <nathan.chevalier@isen-ouest.yncrea.fr>
2025-06-06 11:02:15 +0200

    alert no installation

--- scripts/recherche.js
+++ scripts/recherche.js
@@ -39,10 +39,16 @@ function displayResults(installations) {
   container.innerHTML = ""; // Clear previous results
 
   if(installations.length === 0) {
-    container.innerHTML = "<p class='text-center'>Aucune installation trouvée.</p>";
+container.innerHTML = `
+  <div class="alert alert-info text-center mx-auto mt-4 " role="alert">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+    </svg>
+    Aucune installation trouvée.
+  </div>
+`;
     return;
   }
-  
 
   for(let i = 0; i < installations.length; i++){
     let installation = installations[i];
